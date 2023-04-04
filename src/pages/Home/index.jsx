7e99dcc64cad8dcc1fc3b5fc979121218e854236
Bernardo Camara <bernardocconti@gmail.com>
2023-04-04 15:21:49 -0300

    IMPLEMENTAÇÃO TELA BUSCA CPF

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,9 +5,35 @@ import Header from "../../componentes/Header";
 function Home() {
  
   return (
-    <div className="container">
+    <div className="container-home">
         <Header />
-   
+        <div className="scream-busca-cpf">
+          <div className="container-title">  
+            <h1>NEGOCIE AGORA</h1>
+            <h3>Aproveite, é simples mantenha seu nome limpo.</h3>
+          </div>
+          <div className="container-busca-cpf">
+            <h5>CONSULTE SUAS FATURAS EM ABERTO</h5>
+            <p>Informe abaixo seu CPF/CNPJ ou número do contrato</p>
+            <div className="container-radio-home">
+              <div className="box-radio">
+              <input type="radio"/>
+              <label>CPF/CNPJ</label>
+              </div>
+              <div className="box-radio">
+              <input type="radio"/>
+              <label>Contrato</label>
+              </div>
+            </div>
+            <div className="container-input-home">
+            <input placeHolder="Escreva aqui seu CPF ou seu contrato..."/>
+            <div className="box-lupa">
+            <img id="lupa-icon" src="/img/lupaIconWhite.svg" alt="lupa-home"/>
+            </div>
+            </div>
+          </div>
+           
+        </div>
     </div>
   );
 }
