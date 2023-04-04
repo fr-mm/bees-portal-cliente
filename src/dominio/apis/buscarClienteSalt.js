eee646b0051c2ea8ce13d8164900f305443885d4
chico <chico.zimcolor@gmail.com>
2023-04-04 11:00:43 -0300

    buscarClienteSalt implementado

---
+++ src/dominio/apis/buscarClienteSalt.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export default class APIBuscarClienteSalt {
+  #api;
+
+  constructor() {
+    this.#api = axios.create({
+      baseURL:
+        "https://beesbank.integrador.saltzap.com/webhook/da99f74f-b327-4629-b51f-c686890f0c30/40",
+      timeout: 30000,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  async post({ valor, tipo }) {
+    return await this.#api.post("", JSON.stringify({ valor, tipo }));
+  }
+}
