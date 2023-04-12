4f2fb0a1d917b88ae2628d3494e8583d1cc0ee2a
Ademar Neto <ademarneto47@gmail.com>
2023-04-12 10:08:31 -0300

    COMPONENTIZANDO LOADER

--- src/componentes/ProcessamentoReq/index.jsx
+++ src/componentes/ProcessamentoReq/index.jsx
@@ -1,5 +1,5 @@
 import "./styles.css";
-
+import Loader from "../Loader";
 
 function ProcessamentoReq() {
 
@@ -7,11 +7,7 @@ function ProcessamentoReq() {
     <div className="container-req">
         <div className="box-carregamento">
             <img src="/img/mascotBees.svg" alt="logo-carregamento"/>
-            <div class="loader">
-            <div class="dot dot-1"></div>
-            <div class="dot dot-2"></div>
-            <div class="dot dot-3"></div>
-            </div>
+           <Loader/>
             <h1>Aguarde um momento enquanto <br/>
                 procuramos suas informações.
             </h1>
