4d7cc2229848a78e3dd1037e6d1157257e491c3f
chico <chico.zimcolor@gmail.com>
2023-04-12 22:51:57 -0300

    wait on fake api

--- src/dominio/apis/fakeBackendApi/fakeBackendApi.js
+++ src/dominio/apis/fakeBackendApi/fakeBackendApi.js
@@ -2,6 +2,8 @@ import AbstractAPI from "../abstractApi";
 import fakeBusucarContratos from "./fakeBuscarContratos";
 import fakeSimularAcordo from "./fakeSimularAcordo";
 
+const waitTime = 500;
+
 class FakeResponse {
   status;
   data;
@@ -13,12 +15,18 @@ class FakeResponse {
 
 export default class FakeBackendAPI extends AbstractAPI {
   async buscarContratos({ type, value }) {
+    this.#wait();
     const contratos = fakeBusucarContratos();
     return new FakeResponse(contratos);
   }
 
   async simularAcordo(otdEntrada) {
+    this.#wait();
     const simulacoes = fakeSimularAcordo(otdEntrada);
     return new FakeResponse(simulacoes);
+  }
+
+  async #wait() {
+    await new Promise((resolve) => setTimeout(resolve, waitTime));
   }
 }
